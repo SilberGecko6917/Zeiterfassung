2205e40dc05696b1b2091b9cf61c1d908d87377c
Tobias Schmitt <support@tobfd.de>
2025-11-19 10:59:01 +0100

    fix: update volume path and ensure migration execution in entrypoint (#21)
    * fix: update volume path and ensure migration execution in entrypoint
    
    * fix: update database volume path and add DATABASE_URL environment variable
    
    * Apply suggestions from code review
    
    * Apply suggestion
    
    ---------
    
    Co-authored-by: SilberGecko6917 <109872285+SilberGecko6917@users.noreply.github.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+set -e
+
+cd /app || exit 1
+
+if [ ! -f "/app/data/data.db" ]; then
+    echo "Creating database file..."
+    mkdir -p /app/data
+    touch /app/data/data.db
+fi
+
 echo "Applying Prisma migrations..."
 npx prisma migrate deploy
 
